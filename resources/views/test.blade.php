77a38847f1530a5ea54ccde0d4cfd2cbfe6d3ea0
Niyom Chamaree <niyomja@hotmail.com>
2015-10-11 03:14:24 +0700

    eand

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -8,7 +8,7 @@
     <script src="{ { asset('js/socket.io.js') } }"></script>
     <script>
         //var socket = io('http://localhost:3000');
-        var socket = io('http://192.168.10.10:3000');
+        var socket = io('http://192.168.137.200:3000');
         socket.on("test-channel:App\\Events\\EventName", function(message){
             // increase the power everytime we load test route
             $('#power').text(parseInt($('#power').text()) + parseInt(message.data.power));
